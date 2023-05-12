495f65250282b43ce89ecf48deb4b3c90532c555
dotuanminh <78138604+dotuanminh@users.noreply.github.com>
2023-05-12 12:00:33 +0700

    commit Chat Api

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express'); //
 const userController = require('../controllers/userControlller');
 const authController = require('../controllers/authController');
-const { accessChat } = require('../controllers/chatController');
+const { accessChat, fetchChats, createGroupChat, renameGroup, addToGroup, removeFromGroup } = require('../controllers/chatController');
 
 const router = express.Router();
 
 router.route('/').post(authController.protect, accessChat);
-//router.route('/').get(authController.protect, fetchChats);
-//router.route('/group').post(authController.protect, createGroupChat);
-//router.route('/rename').put(authController.protect, renameGroup);
-//router.route('/groupremove').put(authController.protect, removeFromGroup);
-//router.route('/groupadd').put(authController.protect, addToGroup);
+router.route('/').get(authController.protect, fetchChats);
+router.route('/group').post(authController.protect, createGroupChat);
+router.route('/rename').put(authController.protect, renameGroup);
+router.route('/groupremove').put(authController.protect, removeFromGroup);
+router.route('/groupadd').put(authController.protect, addToGroup);
 
 module.exports = router;
